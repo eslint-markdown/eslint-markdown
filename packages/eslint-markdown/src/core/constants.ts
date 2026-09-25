26e763de07871e60ef5ede1a562883393efdc1b6
Ga eun Lee <hmyo2853@gmail.com>
2026-09-25 21:23:43 +0900

    fix(eslint-markdown): distinguish escaped HTML entities

--- packages/eslint-markdown/src/core/constants.ts
+++ packages/eslint-markdown/src/core/constants.ts
@@ -83,7 +83,7 @@ export const gemojiRegex =
  * @see https://github.com/DavidAnson/markdownlint/blob/v0.41.1/helpers/helpers.cjs#L32-L34
  */
 export const htmlEntityRegex =
-  /&(?:#\d+|#[xX][\da-fA-F]+|[a-zA-Z]{2,31}|blk\d{2}|emsp1[34]|frac\d{2}|sup\d|there4);/;
+  /(?<=(?<!\\)(?:\\{2})*)&(?:#\d+|#[xX][\da-fA-F]+|[a-zA-Z]{2,31}|blk\d{2}|emsp1[34]|frac\d{2}|sup\d|there4);/;
 
 // #endregion regex
 // --------------------------------------------------------------------------------
